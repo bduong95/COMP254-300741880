fb74cd1d4dc6fa615f00ecadb6ec54d47d443a1d
Brian Duong <bduong@Brians-MacBook-Air.local>
2024-01-22 15:36:45 -0500

    Added user input functionality

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 class subscribe {
     public static void main(String[] args)
     {
-        System.out.println("Subscribe");
+        String creator;
+
+        System.out.print("Name of creator? ");
+
+        Scanner myScanner = new Scanner(System.in);
+        creator = myScanner.nextLine();
+
+        System.out.format("Subscribe to %s!\n", creator);
     }
 }
